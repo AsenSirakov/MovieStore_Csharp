f5661aaad7c15dbc05569fd231cd1accea2ddd40
Asen Sirakov <161630566+AsenSirakov@users.noreply.github.com>
2025-06-07 10:20:36 +0300

    Update DependencyInjection.cs

--- MovieStoreB/MovieStoreB.BL/DependencyInjection.cs
+++ MovieStoreB/MovieStoreB.BL/DependencyInjection.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using MovieStoreB.BL.Interfaces;
 using MovieStoreB.BL.Services;
 using MovieStoreB.DL.Interfaces;
@@ -13,11 +13,6 @@ namespace MovieStoreB.BL
             // Register HttpClient for ExternalApiService
             services.AddHttpClient<ExternalApiService>();
 
-            // Register core services - CHOOSE ONE MOVIE SERVICE IMPLEMENTATION
-            // Option 1: Use basic MovieService
-            // services.AddSingleton<IMovieService, MovieService>();
-
-            // Option 2: Use EnhancedMovieService (recommended)
             services.AddSingleton<IMovieService, EnhancedMovieService>();
 
             // Register other services
@@ -30,8 +25,7 @@ namespace MovieStoreB.BL
             // Register ActorBioGateway
             services.AddSingleton<IActorBioGateway, ActorBioGateway>();
 
-            // DO NOT register EnhancedMovieService separately when it's already implementing IMovieService
-            // services.AddSingleton<EnhancedMovieService>(); // REMOVE THIS LINE
+        
 
             return services;
         }
